de8acbd0a07245c529b8ec4739241e56efa730b1
Richard Steward <rsteward@xpansionfts.com>
2020-03-11 14:13:04 +0000

    1.0.1

--- src/implementation/emit-all-event-emitter.ts
+++ src/implementation/emit-all-event-emitter.ts
@@ -3,35 +3,35 @@ import { EventEmitter } from '../definitions/event-emitter';
 import { EventConsumer } from '../definitions/event-consumer';
 
 export class EmitAllEventEmitter<T> implements EventEmitter<T> {
-    private listeners: EventConsumer<T>[] = [];
-    private oneTimeListeners: EventConsumer<T>[] = [];
+  private listeners: EventConsumer<T>[] = [];
+  private oneTimeListeners: EventConsumer<T>[] = [];
 
-    on(listener: EventConsumer<T>): Disposable {
-        this.listeners.push(listener);
-        return {
-            dispose: () => this.off(listener),
-        };
-    }
+  on(listener: EventConsumer<T>): Disposable {
+    this.listeners.push(listener);
+    return {
+      dispose: () => this.off(listener),
+    };
+  }
 
-    once(listener: EventConsumer<T>): void {
-        this.oneTimeListeners.push(listener);
-    }
+  once(listener: EventConsumer<T>): void {
+    this.oneTimeListeners.push(listener);
+  }
 
-    off(listener: EventConsumer<T>) {
-        const callbackIndex = this.listeners.indexOf(listener);
-        if (callbackIndex > -1) this.listeners.splice(callbackIndex, 1);
-    }
+  off(listener: EventConsumer<T>) {
+    const callbackIndex = this.listeners.indexOf(listener);
+    if (callbackIndex > -1) this.listeners.splice(callbackIndex, 1);
+  }
 
-    emit(event: T) {
-        /** Update any general listeners */
-        this.listeners.forEach(listener => listener(event));
+  emit(event: T) {
+    /** Update any general listeners */
+    this.listeners.forEach(listener => listener(event));
 
-        /** Clear the `once` queue */
-        this.oneTimeListeners.forEach(listener => listener(event));
-        this.oneTimeListeners = [];
-    }
+    /** Clear the `once` queue */
+    this.oneTimeListeners.forEach(listener => listener(event));
+    this.oneTimeListeners = [];
+  }
 
-    pipe(te: EventEmitter<T>): Disposable {
-        return this.on(e => te.emit(e));
-    }
+  pipe(te: EventEmitter<T>): Disposable {
+    return this.on(e => te.emit(e));
+  }
 }
